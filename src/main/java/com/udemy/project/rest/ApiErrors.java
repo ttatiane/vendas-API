ae12f92b2d9cbbb313d6e29ed32332bf698fe3da
Tatiane Takeuti <tatiane.takeuti@hotmail.com>
2020-12-03 17:58:41 -0300

    Adicionando dependency Starter Validation e implementando Bean Validation na entidade Cliente

--- src/main/java/com/udemy/project/rest/ApiErrors.java
+++ src/main/java/com/udemy/project/rest/ApiErrors.java
@@ -10,6 +10,10 @@ public class ApiErrors {
     @Getter
     private List<String> erros;
 
+    public ApiErrors(List<String> erros) {
+        this.erros = erros;
+    }
+
     public ApiErrors(String mensagemErro) {
         this.erros = Arrays.asList(mensagemErro);
     }
--- src/main/java/com/udemy/project/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/com/udemy/project/rest/controller/ApplicationControllerAdvice.java
@@ -4,9 +4,13 @@ import com.udemy.project.exception.PedidoNaoEncontradoException;
 import com.udemy.project.exception.RegraNegocioException;
 import com.udemy.project.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -22,6 +26,16 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handlePedidoNotFoundException(PedidoNaoEncontradoException ex) {
         return new ApiErrors(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handlerMethodNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(errors);
     }
 }
 
