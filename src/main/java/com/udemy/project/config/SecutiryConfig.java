3f6fb6db91eb39ee831abf5288fd4c0d3f8c299f
Tatiane Takeuti <tatiane.takeuti@hotmail.com>
2020-12-04 10:35:38 -0300

    Implementando um password encoder

--- src/main/java/com/udemy/project/config/SecutiryConfig.java
+++ src/main/java/com/udemy/project/config/SecutiryConfig.java
@@ -1,12 +1,21 @@
 package com.udemy.project.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecutiryConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // gera hash diferente a cada solicitação
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);  // authentication
