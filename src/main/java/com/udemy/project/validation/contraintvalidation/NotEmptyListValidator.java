046a1979ac9c645a9699df137e5e50cf99e5a488
Tatiane Takeuti <tatiane.takeuti@hotmail.com>
2020-12-04 09:02:58 -0300

    Implementando uma annotation de validação customizada

---
+++ src/main/java/com/udemy/project/validation/contraintvalidation/NotEmptyListValidator.java
@@ -0,0 +1,19 @@
+package com.udemy.project.validation.contraintvalidation;
+
+import com.udemy.project.validation.NotEmptyList;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class NotEmptyListValidator implements ConstraintValidator<NotEmptyList, List> {
+    @Override
+    public boolean isValid(List list, ConstraintValidatorContext context) {
+        return list != null && !list.isEmpty();
+    }
+
+    @Override
+    public void initialize(NotEmptyList constraintAnnotation) {
+
+    }
+}
